d3f5bc9c80a67eaf88d20a6cb40e4aa82c1be9c2
Rick Hills <rh-jobs@outlook.com>
2015-08-14 09:58:35 -0700

    Add PHPUnit tests folder and file, plus Silex, Twig and PHPUnit install requirements

---
+++ src/RepeatCounter.php
@@ -0,0 +1,17 @@
+<?php
+
+    class RepeatCounter
+    {
+ 
+        function countRepeats($string_of_words, $match_word)
+        {
+
+
+
+
+        }
+
+    }
+
+?>
+
---
+++ tests/RepeatCounterTest.php
@@ -0,0 +1,25 @@
+<?php
+
+    require_once "src/RepeatCounter.php";
+
+    class RepeatCounterTest extends PHPUnit_Framework_TestCase
+    {
+
+        function test_countRepeats_noStringOfWords()
+        {
+            //Arange
+            $test_RepeatCounter = new RepeatCounter;
+            $words_string = "";
+            $match_word = "bob";
+
+            //Act
+            $result = $test_RepeatCounter->countRepeats($words_string, $match_word);
+
+            //Assert
+            $this->assertEquals(0, $result);
+
+        }
+
+    }
+
+?>
